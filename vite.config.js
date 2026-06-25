58313424f002cbdacb9cd701a1e57d929ba38be7
forzzze <nikitmuz@gmail.com>
2026-06-25 12:21:12 +0300

    +privacy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        privacy: resolve(__dirname, "privacy.html"),
+      },
+    },
+  },
   test: {
     environment: "jsdom",
   },
